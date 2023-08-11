200b16125faedb3e1949ee4b2dc7811ecaf617c4
Olivet Café <131605303+OlivetCafe@users.noreply.github.com>
2023-08-11 15:57:14 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
 function addPicture(pic) {
-    document.getElementById(pic).innerHTML = "<img src='imgs/"+pic+".jpg' />";
+    if (document.getElementById(pic).childNodes.length === 0 ){
+        document.getElementById(pic).innerHTML = "<img src='imgs/"+pic+".jpg' />";
+        return;
+    }
+    document.getElementById(pic).innerHTML = "";
 }
