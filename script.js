b085b6f95502b67863648a2666719e6a47aca75c
Olivet Café <131605303+OlivetCafe@users.noreply.github.com>
2023-10-18 17:48:56 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function addPicture(pic) {
-    if (document.getElementById(pic).childNodes.length === 0 ){
-        document.getElementById(pic).innerHTML = "<img src='imgs/"+pic+".jpg' />";
-        return;
-    }
-    document.getElementById(pic).innerHTML = "";
-}
